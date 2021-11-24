27a7f3001143fbe8210a30e98dc0db26c6a52b4e
ananya mehta <93441557+ananyamehtax@users.noreply.github.com>
2021-11-24 11:01:38 -0500

    Add files via upload

---
+++ if statements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  if (9>2) {
+    cout << "MATERIAL GWORLL\n";
+  }
+  if (2<6) {
+    cout << "the queen is a MATERIAL GWORLL\n";
+  }
+  if (6==6) {
+    cout << "you're a MATERIAL GWORLL\n";
+  }
+  int a = 99;
+  int b = 66;
+    if (a>b) {
+      cout << "everyone's a MATERIAL GWORLL\n";
+    if (a>9)
+      cout << "i love MATERIAL GWORLLS\n";
+    if (b==66)
+      cout << "<3";
+    }
+  return 0;
+} 
