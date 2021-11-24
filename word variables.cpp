b6b3fc4d17d7450f0c26aee712f200f232f70f8d
ananya mehta <93441557+ananyamehtax@users.noreply.github.com>
2021-11-24 11:14:11 -0500

    Add files via upload

---
+++ word variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string x = "jake";
+  string y = "stole";
+  string z = "taylor's scarf\n";
+  cout << x <<" "<< y <<" " << z;
+ 
+  string c = "cheerios";
+  string b = "love ";
+  string a = "i ";
+  cout << a << b << c;
+}
