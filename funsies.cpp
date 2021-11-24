edad23c9609668b0e5492828f0f821baa22dc3f5
ananya mehta <93441557+ananyamehtax@users.noreply.github.com>
2021-11-24 18:27:46 -0500

    Add files via upload
    for the swifties only :3

---
+++ funsies.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "I walked through the door with you\nThe air was cold\nBut something about it felt like home somehow\nAnd I, left my scarf there at your sisters house\nAnd youve still got it in your drawer even now\n\n";
+  cout << "Oh, your sweet disposition\nAnd my wide-eyed gaze\nWe are singing in the car, getting lost upstate\nAutumn leaves falling down like pieces into place\nAnd I can picture it after all these days\n\n";
+  cout << "And I know it's long gone and that magic's not here no more\nAnd I might be okay but Im not fine at all\n\n";
+  cout << "Cause there we are again on that little town street\nYou almost ran the red 'cause you were lookin' over at me\nWind in my hair, I was there\nI remember it all too well\n\n";
+  cout << "Photo album on the counter\nYour cheeks were turning red\nYou used to be a little kid with glasses in a twin-sized bed\nAnd your mothers telling stories bout you on the tee-ball team\nYou told me bout your past thinking your future was me\n\n";
+  cout << "And I know its long gone and there was nothing else I could do\nAnd I forget about you long enough to forget why I needed to\n\n";
+  cout << "Cause there we are again in the middle of the night\nWe are dancing round the kitchen in the refrigerator light\nDown the stairs, I was there\nI remember it all too well, yeah\n\n";
+  cout << "And maybe we got lost in translation\nMaybe I asked for too much\nBut maybe this thing was a masterpiece til you tore it all up\nRunning scared, I was there\nI remember it all too well\n\n";
+  cout << "And you call me up again just to break me like a promise\nSo casually cruel in the name of being honest\nIm a crumpled up piece of paper lying here\nCause I remember it all, all, all\nToo well\n\n";
+  cout << "Time wont fly, its like Im paralyzed by it\nId like to be my old self again\nBut Im still trying to find it\nAfter plaid shirt days and nights when you made me your own\nNow you mail back my things and I walk home alone\nBut you keep my old scarf from that very first week\nCause it reminds you of innocence\nAnd it smells like me\nYou cant get rid of it\nCause you remember it all too well, yeah\n\n";
+  cout << "Cause there we are again when I loved you so\nBack before you lost the one real thing youve ever known\nIt was rare, I was there, I remember it all too well\n\n";
+  cout << "Wind in my hair, you were there, you remember it all\nDown the stairs, you were there, you remember it all\nIt was rare, I was there, I remember it all too well\n\n";
+} 
